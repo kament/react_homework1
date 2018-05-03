59c8563d4a884973caf85f7e37d83d06ed94ab13
Kamen <k.todorov@ultraplay.co>
2018-05-04 00:06:56 +0300

    create blog form

--- src/blog-app.js
+++ src/blog-app.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import BlogsList from './blogs-list';
+import CreateBlog from './create-blog'
 
 export default class BlogApp extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
             blogs: [
                 { id: 1, header: 'Post 1', text: 'Text 1', autor: 'Kamen' },
@@ -13,13 +15,21 @@ export default class BlogApp extends React.Component {
                 { id: 5, header: 'Post 5', text: 'Text 5', autor: 'Kamen' },
             ]
         };
-    }
+    };
 
     render() {
         return (
             <div>
+                <CreateBlog onCreateBlog={this.onCreateBlog}/>
                 <BlogsList blogs={this.state.blogs} />
             </div>
         );
+    };
+
+    onCreateBlog = (e, blog) => {
+        e.preventDefault();
+        console.log("Add");
+        console.dir(e);
+        console.dir(blog);
     };
 }
--- src/create-blog.js
+++ src/create-blog.js
@@ -1,12 +1,52 @@
-// import React from 'react';
+import React from 'react';
 
-// export default class CreateBlog extends React.Component {
-    
-//     render() (
-//         <form>
-//             <div>
-//                 <input  />
-//             </div>
-//         </form>
-//     );
-// };
+export default class CreateBlog extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            date: Date.now(),
+            title: '',
+            autor: '',
+            text: '',
+            tags: [],
+            url: '',
+            status: 'active'
+        };
+    }
+
+    render() {
+        return (
+            <form onSubmit={(e) => this.props.onCreateBlog(e, this.state)}>
+                <div>
+                    <label htmlFor="title">Blog title</label>
+                    <input type="text" name="title"/>
+                </div>
+                <div>
+                    <label htmlFor="autor">Autor</label>
+                    <input type="text" name="autor"/>
+                </div>
+                <div>
+                    <label htmlFor="text">Text of the blog post</label>
+                    <input type="text" name="text"/>
+                </div>
+                <div>
+                    <label htmlFor="tags">Tags</label>
+                    <input type="text" name="tags"/>
+                </div>
+                <div>
+                    <label htmlFor="url">Image url</label>
+                    <input type="text" name="url"/>
+                </div>
+                <div>
+                    <label htmlFor="title">Status</label>
+                    <input type="radio" name="status" value="active" checked />
+                    <input type="radio" name="status" value="inactive" />
+                </div>
+                <div>
+                    <input type="submit" value="Create"/>
+                </div>
+            </form>
+        );
+    };
+};
